82abde54a33e8bef84a9556665ce08cfeddaa7dc
Emmanuel Obagunwa <access2emma@gmail.com>
2025-01-08 23:01:02 +0100

    unit test and e2e test

--- test/coffee/coffee.e2e-spec.ts
+++ test/coffee/coffee.e2e-spec.ts
@@ -1,10 +1,19 @@
-import { INestApplication } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
-import { CoffeesModule } from '../../src/coffees/coffees.module';
+import * as request from 'supertest';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+
+import { CoffeesModule } from '../../src/coffees/coffees.module';
+import { CreateCoffeeDto } from 'src/coffees/dto/create.coffee.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+
+  const coffee = {
+    name: 'Coffee 1',
+    brand: 'Brand 1',
+    flavors: ['Flavor 1', 'Flavor 2'],
+  };
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -24,11 +33,54 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+
     await app.init();
   });
 
-  it.todo('Create [POST /]');
-  it.todo('Get all [GET /]');
+  it('Create [POST /]', () => {
+    return request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED)
+      .then(({ body }) => {
+        expect(body).toMatchObject({ ...coffee, flavors: expect.any(Array) });
+      });
+  });
+
+  it('Get all [GET /]', () => {
+    return request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .then(({ body }) => {
+        expect(body.id).toBeDefined();
+
+        return request(app.getHttpServer())
+          .get('/coffees?page=1&size=20')
+          .expect(HttpStatus.OK)
+          .then((response) => {
+            expect(response.body).toEqual(
+              expect.arrayContaining([
+                expect.objectContaining({
+                  id: body.id,
+                  ...coffee,
+                  flavors: expect.any(Array),
+                }),
+              ]),
+            );
+          });
+      });
+  });
+
   it.todo('Get one (GET /:id)');
   it.todo('Delete One  [DELETE /:id]');
   it.todo('Update One [PATCH /:id]');
